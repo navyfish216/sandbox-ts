e2419685dfc18051d7bee930b7b9b4ff3feb9b8e
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-06-08 20:55:12 +0900

    【TypeScript】修行①

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello, TypeScript!!");
+let age:number = 0;
+age = 35;
+const pai:number = 3.14;
+console.log(age + pai);
+
+const msg1:string = "Hello";
+const msg2:string = 'Hello';
+const msg3:string = `Hello`;
+console.log(msg1);
+console.log(msg2);
+console.log(msg3);
+
+const calc = `1 + 2 = ${1 + 2}`;
+console.log(calc);
+
+const user:string = "HIRO";
+const msg:string = `Hello ${user} さん`;
+console.log(msg);
+
+const isYes:boolean = true;
+const isNo:boolean = false;
+
+//const x:null = null;
+//console.log(x);
+
+let x;
+let y:undefined = undefined;
+console.log(x);
+console.log(y);
+
+console.log(typeof(age + pai));
+console.log(typeof(msg));
+console.log(typeof(isYes));
+
+// タプル型変数
+const userdata:[string, number, number, number] = ["HIRO", 35, 175.2, 65.3];
+console.log(userdata[0]);
+console.log(userdata[1]);
+console.log(userdata[2]);
+console.log(userdata[3]);
