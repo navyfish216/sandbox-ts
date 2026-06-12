ba40ed615a3b034b8998c7fc2c362c41f94905af
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-06-12 21:53:41 +0900

    【TypeScript】修行⑤追加(配列回りの処理を色々試してみた)

--- src/index_6.ts
+++ src/index_6.ts
@@ -1,6 +1,6 @@
 // 6章:繰り返し処理
 
-const kokugo = [80, 65, 90, 75, 85];
+let kokugo = [80, 65, 90, 75, 85];
 
 let sum = 0;
 for (let i = 0; i < kokugo.length; i++) {
@@ -16,3 +16,29 @@ for (let num of kokugo){
 }
 avarage = sum / kokugo.length;
 console.log(`${kokugo.length}人の国語の合計点数は${sum}点で、平均点数は${avarage}点です。`);
+
+console.log();
+console.log("-- kokugo.map --");
+kokugo.map((k) => console.log(k));
+
+console.log();
+console.log("-- kokugo.find --");
+let findResult = kokugo.find((k) => (k >= 85 ? true : false));
+console.log(`findResult : ${findResult}`);
+findResult = kokugo.find((k) => (k > 90 ? true : false)); // 条件に合致しないとundefinedになる
+console.log(`findResult : ${findResult}`);
+
+console.log();
+console.log("-- kokugo.filter --");
+let filterResult: number[] = kokugo.filter((k) => (k >= 85 ? true : false));
+console.log(`filterResult.length : ${filterResult.length}`);
+filterResult.map((f) => console.log(f));
+console.log();
+filterResult = kokugo.filter((k) => (k > 90 ? true : false)); // 条件に合致しないと0件になる
+console.log(`filterResult.length : ${filterResult.length}`);
+filterResult.map((f) => console.log(f));
+
+console.log();
+console.log("-- kokugo.map --");
+kokugo = [33, ...kokugo, 99];
+kokugo.map((k) => console.log(k));
